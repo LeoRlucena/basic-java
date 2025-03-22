f46201459262c0a72c3d669b3d6926633f647890
Leonardo <leorlucena@gmail.com>
2025-03-22 13:31:01 -0300

    first primitive data types

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+
+    public static void main(String[] args) {
+
+        int xyz = 20;
+
+        int abc = 2;
+        int v = abc + 1; //3
+
+        boolean b1 = true;
+        boolean b2 = false;
+
+        double d1 = 10.2;
+        double d2 = 20.5;
+    }
+}
