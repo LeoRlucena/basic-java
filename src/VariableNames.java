2994aff5cab8973a791de5bddc770978f77b1ec5
Leonardo <leorlucena@gmail.com>
2025-03-22 13:49:23 -0300

    Variable names conventions

---
+++ src/VariableNames.java
@@ -0,0 +1,21 @@
+public class VariableNames {
+
+    public static void main(String[] args) {
+
+        int abc;
+        // int 0abc; ERRO
+        int abc42;
+        int $var;
+        int _var;
+        int va$r;
+        int va__r;
+        // int xp#f; ERRO
+
+        int house;  // Follows convention
+        int House;
+        int HOUSE;  // Constants convention
+
+        int numberOfYears;  // Camel-case
+        int number_of_years;  // Uncommon
+    }
+}
