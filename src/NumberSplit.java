ff5c7b74d2f1aee8ac23aa24ea6ab0ad545f6e9d
Leonardo <leorlucena@gmail.com>
2025-03-22 14:07:03 -0300

    Number split in Java

---
+++ src/NumberSplit.java
@@ -0,0 +1,15 @@
+public class NumberSplit {
+
+    public static void main(String[] args) {
+
+        int n = 476_879_386;
+        System.out.println(n);
+
+        double d = 1_023_801_928_301.1_2_0_3_7_8;
+        System.out.println(d);
+
+        // int x1 = _10;
+        // int x2 = 10_;
+        // double x3 = 10_.3;
+    }
+}
