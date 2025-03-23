39607781ca52ed3b057871722e8f14a8423f68c6
Leonardo <leorlucena@gmail.com>
2025-03-23 20:17:56 -0300

    implicit casting in Java

---
+++ src/ImplicitCasting.java
@@ -0,0 +1,15 @@
+public class ImplicitCasting {
+
+    public static void main(String[] args) {
+
+        int i = 200;
+        double d = i;    //implicit casting.
+
+        System.out.println(i);
+        System.out.println(d);
+
+        double d2 = 200L;  //implicit casting.
+
+        long l = 500L;
+    }
+}
