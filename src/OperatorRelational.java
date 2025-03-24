7c06203ded44e8c0b52318b293a4d42a1e14e4e7
Leonardo <leorlucena@gmail.com>
2025-03-24 13:09:29 -0300

    relational operator in Java

---
+++ src/OperatorRelational.java
@@ -0,0 +1,16 @@
+public class OperatorRelational {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        boolean r = a == b;
+        System.out.println(r);
+
+        int c = 20;
+        System.out.println(a == c);
+        System.out.println(a != c);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+    }
+}
