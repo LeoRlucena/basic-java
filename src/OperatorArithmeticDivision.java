f47f8ec572ec247a66fa99149c5ecb1da67dd5ee
Leonardo <leorlucena@gmail.com>
2025-03-24 13:00:02 -0300

    division arithmetic operator in Java

---
+++ src/OperatorArithmeticDivision.java
@@ -0,0 +1,10 @@
+public class OperatorArithmeticDivision {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 2;
+        double c = (double) b / a;
+        System.out.println(c);
+    }
+}
