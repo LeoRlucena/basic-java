895fdd16ff99502574486a3817f162deff9c6816
Leonardo <leorlucena@gmail.com>
2025-03-30 14:27:26 -0300

    multiplication table exercise

---
+++ src/ExTabuada.java
@@ -0,0 +1,15 @@
+public class ExTabuada {
+
+    public static void main(String[] args) {
+
+        var scanner = new java.util.Scanner(System.in);
+
+        System.out.print("Digite um número para gerar a tabuada: ");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+
+    }
+}
