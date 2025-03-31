d5a14c0568ec4c3911d9361f75e54a67db9c7e6a
Leonardo <leorlucena@gmail.com>
2025-03-31 14:42:05 -0300

    commenting on IntelliJ

--- src/App.java
+++ src/App.java
@@ -1,12 +1,21 @@
 public class App {
 
     public static void main(String[] args) {
+        // Comentário
         int x = 2;
         int y = x + 5;
+        // FIXME corrigir isso aqui
 
         while (y < 10) {
             System.out.println(y);
             y += 2;
+            // TODO Voltar aqui depois
         }
+
+
+        /*asdasd
+                asd
+                asd
+                        asd*/
     }
 }
