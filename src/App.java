c161a8afb6a9cde869e7872b02cac300cd532133
Leonardo <leorlucena@gmail.com>
2025-03-31 14:24:34 -0300

    learning debug on IntelliJ

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+        int x = 2;
+        int y = x + 5;
+
+        while (y < 10) {
+            System.out.println(y);
+            y += 2;
+        }
+    }
+}
