995e667cf85654f10a95f6b3abb5e6e04a11c2e1
Leonardo <leorlucena@gmail.com>
2025-04-01 13:27:57 -0300

    learning about javadoc

---
+++ src/Javadoc.java
@@ -0,0 +1,36 @@
+/**
+ * Classe do meu programa
+ *
+ * @author Leonardo Lucena
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Javadoc {
+
+    /**
+     * Metodo main.
+     * @param args Parâmetros de linha de comando
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Este é um metodo.
+     * @param x O número
+     */
+    public static void m1(int x) {
+
+    }
+
+    /**
+     * Este é outro metodo.
+     * @param n1 Numero 1.
+     * @param n2 Numero 2.
+     * @return Por enquanto, true.
+     */
+    private static boolean m2(double n1, double n2) {
+        return true;
+    }
+}
